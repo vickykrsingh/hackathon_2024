2ff539832d22e72a3eeed7abc37ce1940b6f15b2
Dhiraj Shah <shahdhiraj0004@gmail.com>
2024-04-13 18:49:16 +0530

    remove answer

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,10 +24,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    answer: {
-      type: String,
-      required: true,
-    },
+    
     role: {
       type: Number,
       default: 0,
